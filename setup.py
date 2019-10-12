c50bfac28ac982fbb4577e82bf0d84eb5e778885
catarina <c.alexandracorreia@ua.pt>
2019-10-12 22:45:52 +0100

    Initial code release

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="webcache",
+    version="0.0.1",
+    author="Catarina Silva",
+    author_email="c.alexandracorreia@ua.pt",
+    description="Web cache library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/catarinaacsilva/web-cache",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=['selenium>=3.141.0'],
+)
---
+++ webcache/webcache.py
@@ -0,0 +1,75 @@
+# coding: utf-8
+
+
+__author__ = 'Catarina Silva'
+__version__ = '0.0.1'
+__email__ = 'c.alexandracorreia@ua.pt'
+__status__ = 'Development'
+
+
+import logging
+import os
+import hashlib
+import time
+import gzip
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+
+
+def fnv1a_64(string: str, seed=0: int):
+	"""
+	Returns: The FNV-1a (alternate) hash of a given string
+	"""
+	#Constants
+	FNV_prime = 1099511628211
+	offset_basis = 14695981039346656037
+
+	#FNV-1a Hash Function
+	hash = offset_basis + seed
+	for char in string:
+		hash = hash ^ ord(char)
+ 		hash = hash * FNV_prime
+	return hash
+
+
+def load_url(url: str, path: str, driver: webdriver):
+    file_name = '{}/{}.gz'.format(self.path, fnv1a_64(url))
+    html = driver.execute_script("return document.getElementsByTagName('html')[0].innerHTML")
+    with gzip.open(file_name, 'wb') as f:
+        f.write(html)
+    return html
+
+
+class WebCache(object):
+    def __init__(self, path='/tmp/webcache': str, ttl=24*3600: int):
+        self.path = path
+        self.ttl = ttl
+        options = Options()
+        options.headless = True
+        self.driver = webdriver.Firefox(options=options)
+        self.hash = hashlib.sha128
+        if not os.path.exists(self.path):
+            os.makedirs(self.path)
+        
+    def get(url: str, refresh=False: bool):
+        file_name = '{}/{}.gz'.format(self.path, fnv1a_64(url))
+        if os.path.exists(file_name):
+            creation_time = time.ctime(os.path.getmtime(file_name))
+            alive_time = time.time()-creation_time
+            if alive_time > ttl:
+                html = load_url(url, self.path, self.driver)
+            else:
+                html = None
+                with gzip.open(file_name, 'rb') as f:
+                    html = f.read()
+        else:
+            html = load_url(url, self.path, self.driver)
+
+        return html
+
+    def __del__(self):
+        self.driver.stop_client()
+        self.driver.close()
+
+
+    
